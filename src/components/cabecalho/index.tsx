816b166c6c472e5220ad9ec55f27dff652278051
anamagaroti <ficava123@gmail.com!git config --global user.email ficava123@gmail.com>
2024-09-24 09:17:42 -0300

    add

--- src/components/cabecalho/index.tsx
+++ src/components/cabecalho/index.tsx
@@ -3,8 +3,10 @@ import logo from '../../images/logo.svg';
 import './style.css';
 
 function Header() {
+  // Estado para controlar a visibilidade do menu
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
+  // Função para alternar o estado do menu
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -15,17 +17,18 @@ function Header() {
         <img src={logo} alt="logo da empresa magcros, letras brancas e icone na cor azul, tipo um espiral" />
       </div>
 
-      {/* Botão Hamburger para telas menores */}
+      {/* Botão Hamburger para telas menores
       <button className="menu-button" onClick={toggleMenu}>
         ☰
-      </button>
+      </button> */}
 
-      <div className={`links-menu-div ${isMenuOpen ? 'open' : ''}`}>
-        <a className="links-menu" href="https://google.com">O que é a Magcros?</a>
-        <a className="links-menu" href="https://google.com">Soluções</a>
-        <a className="links-menu" href="https://google.com">Para Empresas</a>
+      {/* Links do menu, que ficam escondidos em telas pequenas */}
+      {/* <div className={`links-menu-div ${isMenuOpen ? 'open' : ''}`}>
+        <a className='links-menu' href="https://google.com">O que é a Magcros?</a>
+        <a className='links-menu' href="https://google.com">Soluções</a>
+        <a className='links-menu' href="https://google.com">Para Empresas</a>
         <button type="button">Login</button>
-      </div>
+      </div> */}
     </header>
   );
 }
